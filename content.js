32f6f6e41bd4189808dfd9f5e7ad8ecf061b4a84
darakcheev00 <daniel.arakcheev@gmail.com>
2024-02-08 07:55:46 -0500

    added customs

--- content.js
+++ content.js
@@ -1,45 +1,77 @@
 
+setCustomColorInStorage = async (index, newColor) => {
+	if (index == 1){
+		await chrome.storage.local.set({ customColor1: newColor }, () => {
+			console.log(`customColor1 saved in chrome storage ${newColor}`);
+		});
+	} else if (index == 2){
+		await chrome.storage.local.set({ customColor2: newColor }, () => {
+			console.log(`customColor2 saved in chrome storage ${newColor}`);
+		});
+	} else if (index == 3){
+		await chrome.storage.local.set({ customColor3: newColor }, () => {
+			console.log(`customColor3 saved in chrome storage ${newColor}`);
+		});
+	}
+}
+
+getCustomColorFromStorage = async (index) => {
+	const key = `customColor${index}`;
+	return new Promise((resolve) => {
+		chrome.storage.local.get([key], (data) => {
+			const customColor = data[key] || '#343541'; // Provide a default color if not found
+			console.log(`Retrieved ${key} from storage: ${customColor}`);
+			resolve(customColor);
+		});
+	});
+};
+
+
 setGPTColorInStorage = async (newColor) => {
-    await chrome.storage.local.set({ gptColor: newColor }, () => {
-        console.log(`New gpt background color saved in chrome storage ${newColor}`);
-    });
+	await chrome.storage.local.set({ gptColor: newColor }, () => {
+		console.log(`New gpt background color saved in chrome storage ${newColor}`);
+	});
 };
 
 getGPTColorFromStorage = async () => {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(['gptColor'], (data) => {
-            const gptColor = data.gptColor || '#355751'; // Provide a default color if not found
-            console.log(`Retrieved gpt background color from storage: ${gptColor}`);
-            resolve(gptColor);
-        });
-    });
+	return new Promise((resolve) => {
+		chrome.storage.local.get(['gptColor'], (data) => {
+			const gptColor = data.gptColor || '#343541'; // Provide a default color if not found
+			console.log(`Retrieved gpt background color from storage: ${gptColor}`);
+			resolve(gptColor);
+		});
+	});
 };
 
 
 const setColor = (newColor) => {
-    var styleElement = document.createElement('style');
+	var styleElement = document.createElement('style');
 
-    styleElement.textContent = `
+	styleElement.textContent = `
             :root.dark {
                 --main-surface-primary: ${newColor};
             }
             `;
 
-    document.head.appendChild(styleElement);
+	document.head.appendChild(styleElement);
 }
 
 (async () => {
-    const colorFromStorage = await getGPTColorFromStorage();
-    setColor(colorFromStorage);
+	const colorFromStorage = await getGPTColorFromStorage();
+	setColor(colorFromStorage);
 
-    chrome.runtime.onMessage.addListener(
-        async function (request, sender, sendResponse) {
-            if (request.action === 'changeColor') {
-                setColor(request.color);
-                await setGPTColorInStorage(request.color);
-            }
-        }
-    );
+	chrome.runtime.onMessage.addListener(
+		async function (request, sender, sendResponse) {
+			if (request.action === 'changeColor') {
+				console.log(`setting and saving background color:${request.color}`);
+				setColor(request.color);
+				await setGPTColorInStorage(request.color);
+			} else if (request.action === 'saveCustomColor') {
+				console.log(`saving custom color index:${request.customColorIndex}, color:${request.color}`);
+				await setCustomColorInStorage(request.customColorIndex, request.color);
+			}
+		}
+	);
 })();
 
 
--- popup.js
+++ popup.js
@@ -1,67 +1,101 @@
 document.addEventListener('DOMContentLoaded', async function () {
-  // This ensures that the DOM is fully loaded before attempting to access elements
+    // This ensures that the DOM is fully loaded before attempting to access elements
+    
+    // 1 ===================================================================
+    var colorPicker1 = document.getElementById(`colorPicker1`);
+    colorPicker1.value = await getCustomColorFromStorage(1);
+    var changeColorBtn1 = document.getElementById(`changeColorBtn1`);
 
-  // Get the color picker element
-  var colorPicker = document.getElementById('colorPicker');
-  const defaultColor = await getGPTColorFromStorage();
-  colorPicker.value = defaultColor;
+    if (changeColorBtn1) {
+        changeColorBtn1.addEventListener('click', function () {
+            var selectedColor = colorPicker1.value;
+            console.log(`using custom color index: 1, color:${selectedColor}`);
 
-  // Get the change color button element
-  var changeColorBtn = document.getElementById('changeColorBtn');
-
-  // Add event listener only if the elements are found
-  if (changeColorBtn) {
-    changeColorBtn.addEventListener('click', function () {
-      // Get the selected color from the color picker
-      var selectedColor = colorPicker.value;
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        // Send the selected color to the content script
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: selectedColor });
-      });
-    });
-  }
-
-  // Function to set the color from a preset circle
-  window.setPresetColor = function (color) {
-    var formattedColor = rgbToHex(color);
-    console.log(`the color: ${formattedColor}`);
-    colorPicker.value = formattedColor;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: formattedColor });
-    });
-  };
-
-  // Add event listeners to the color circles dynamically
-  var colorCircles = document.getElementById('colorCircles');
-  colorCircles.addEventListener('click', function (event) {
-    var target = event.target;
-    if (target.classList.contains('color-circle')) {
-      var chosenColor = target.style.backgroundColor;
-      console.log(`chosen color: ${chosenColor}`);
-      setPresetColor(chosenColor);
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: selectedColor });
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'saveCustomColor', customColorIndex: 1, color: selectedColor });
+            });
+        });
     }
-  });
+
+    // 2 ===================================================================
+    var colorPicker2 = document.getElementById(`colorPicker2`);
+    colorPicker2.value = await getCustomColorFromStorage(2);
+    var changeColorBtn2 = document.getElementById(`changeColorBtn2`);
+
+    if (changeColorBtn2) {
+        changeColorBtn2.addEventListener('click', function () {
+            var selectedColor = colorPicker2.value;
+            console.log(`using custom color index: 2, color:${selectedColor}`);
+
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: selectedColor });
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'saveCustomColor', customColorIndex: 2, color: selectedColor });
+            });
+        });
+    }
+
+
+    // 3 ===================================================================
+    var colorPicker3 = document.getElementById(`colorPicker3`);
+    colorPicker3.value = await getCustomColorFromStorage(3);
+    var changeColorBtn3 = document.getElementById(`changeColorBtn3`);
+
+    if (changeColorBtn3) {
+        changeColorBtn3.addEventListener('click', function () {
+            var selectedColor = colorPicker3.value;
+            console.log(`using custom color index: 3, color:${selectedColor}`);
+
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: selectedColor });
+                chrome.tabs.sendMessage(tabs[0].id, { action: 'saveCustomColor', customColorIndex: 3, color: selectedColor });
+            });
+        });
+    }
+
+
+    // Function to set the color from a preset circle
+    window.setPresetColor = function (color) {
+        var formattedColor = rgbToHex(color);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'changeColor', color: formattedColor });
+        });
+    };
+
+    // Add event listeners to the color circles dynamically
+    var colorCircles = document.getElementById('colorCircles');
+    colorCircles.addEventListener('click', function (event) {
+        var target = event.target;
+        if (target.classList.contains('color-circle')) {
+            var chosenColor = target.style.backgroundColor;
+            console.log(`chosen color: ${chosenColor}`);
+            setPresetColor(chosenColor);
+        }
+    });
 });
 
 
-getGPTColorFromStorage = async () => {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(['gptColor'], (data) => {
-      const gptColor = data.gptColor || '#343541'; // Provide a default color if not found
-      console.log(`Retrieved gpt background color from storage: ${gptColor}`);
-      resolve(gptColor);
+getCustomColorFromStorage = async (index) => {
+    const key = `customColor${index}`;
+    return new Promise((resolve) => {
+        chrome.storage.local.get([key], (data) => {
+            console.log(data);
+            const customColor = data[key] || '#000000'; // Provide a default color if not found
+            console.log(`Retrieved ${key} from storage: ${customColor}`);
+            resolve(customColor);
+        });
     });
-  });
 };
 
+
 function rgbToHex(rgb) {
-  // Convert the RGB color to hex format
-  var match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  if (!match) {
-    return rgb; // Return original color if not in RGB format
-  }
-  var hex = '#' + ('0' + parseInt(match[1], 10).toString(16)).slice(-2) +
-             ('0' + parseInt(match[2], 10).toString(16)).slice(-2) +
-             ('0' + parseInt(match[3], 10).toString(16)).slice(-2);
-  return hex;
+    // Convert the RGB color to hex format
+    var match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    if (!match) {
+        return rgb; // Return original color if not in RGB format
+    }
+    var hex = '#' + ('0' + parseInt(match[1], 10).toString(16)).slice(-2) +
+        ('0' + parseInt(match[2], 10).toString(16)).slice(-2) +
+        ('0' + parseInt(match[3], 10).toString(16)).slice(-2);
+    return hex;
 }
